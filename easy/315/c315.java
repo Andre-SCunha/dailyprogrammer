ca582232aae7189e5f2611cef0830b32d7c2f58b
Andre-SCunha <brayils.andre@gmail.com>
2017-07-11 02:48:18 -0300

    Finished

--- easy/315/c315.java
+++ easy/315/c315.java
@@ -1,19 +1,39 @@
-import java.io.*;
 import java.util.*;
 
 public class c315 {
 
 	private static int xorMultiplyer (int a, int b){
+		int div, op;
+		//Selecting the smaller one to be the one iterating over
+		if(a<b){
+			div = a;
+			op = b;
+		}
+		else{
+			div = b;
+			op = a;
+		}
+		int res = 0;
 		
-		return 0;
+		//Iterating over the bits of div, and applying the xor
+		while(div > 0){
+			res ^= op * (div%2);
+			div /= 2;
+			op *= 2;
+		}
+		
+		return res;
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = xorMultiplyer(a,b);
-		System.out.println(a + "@" + b + "=" + c);
+		while(sc.hasNextInt()){
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = xorMultiplyer(a,b);
+			System.out.println(a + "@" + b + "=" + c);	
+		}
+		sc.close();
 	}
 
 }
