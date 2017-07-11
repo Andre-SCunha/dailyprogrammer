14909152c855489a25ff6313625487d10308d6f0
Andre-SCunha <brayils.andre@gmail.com>
2017-07-11 02:08:32 -0300

    new method

---
+++ easy/315/c315.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class c315 {
+
+	private static int xorMultiplyer (int a, int b){
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
