045c099ef1ef1ed279844838aff86cef8d4647c7
Andre-SCunha <brayils.andre@gmail.com>
2017-07-11 02:14:45 -0300

    att

--- easy/315/c315.java
+++ easy/315/c315.java
@@ -9,8 +9,11 @@ public class c315 {
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = xorMultiplyer(a,b);
+		System.out.println(a + "@" + b + "=" + c);
 	}
 
 }
